2d159e16f150729f2ca515964002c5aab8baa65e
Mikha <mikharohrs@gmail.com>
2023-03-07 16:15:15 +0000

    Merged the GetLocomotionType and PlayWalkingSoundEffect scripts
    Made the latter script also perform the functions of the former.

--- VR Locomotion Test/Assets/Scripts/PlayWalkingSoundEffect.cs
+++ VR Locomotion Test/Assets/Scripts/PlayWalkingSoundEffect.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayWalkingSoundEffect : MonoBehaviour
 {
@@ -7,21 +9,68 @@ public class PlayWalkingSoundEffect : MonoBehaviour
 
     [SerializeField] private InputActionProperty leftJoystick;
     [SerializeField] private InputActionProperty rightJoystick;
+
+    [SerializeField] private InputActionProperty leftActivate;
+    [SerializeField] private InputActionProperty rightActivate;
+    
+    [SerializeField] private InputActionProperty leftCancel;
+    [SerializeField] private InputActionProperty rightCancel;
+    
+    private enum LocomotionType
+    {
+        Teleport,
+        Joystick,
+        Armswing
+    }
+    
+    private LocomotionType _locomotion;
     
     private void Start()
     {
         _soundEffect = GetComponent<AudioSource>();
+        
+        if (gameObject.GetComponent<ActionBasedContinuousMoveProvider>().enabled)
+        {
+            _locomotion = LocomotionType.Joystick;
+        }
+        else if (gameObject.GetComponent<TeleportationProvider>().enabled)
+        {
+            _locomotion = LocomotionType.Teleport;
+        }
+        else if (gameObject.GetComponent<ArmswingMoveProvider>().enabled)
+        {
+            _locomotion = LocomotionType.Armswing;
+        }
     }
     
     private void Update()
     {
-        if (leftJoystick.action.ReadValue<Vector2>() != Vector2.zero || rightJoystick.action.ReadValue<Vector2>() != Vector2.zero)
+        switch (_locomotion)
         {
-            _soundEffect.enabled = true;
-        }
-        else
-        {
-            _soundEffect.enabled = false;
+            case LocomotionType.Joystick when leftJoystick.action.ReadValue<Vector2>() != Vector2.zero || rightJoystick.action.ReadValue<Vector2>() != Vector2.zero:
+                _soundEffect.enabled = true;
+                GameManager.instance.CalculatePlayerSpeed();
+                break;
+            case LocomotionType.Joystick:
+                _soundEffect.enabled = false;
+                break;
+            case LocomotionType.Teleport when leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f || rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f:
+                _soundEffect.enabled = true;
+                GameManager.instance.CalculatePlayerSpeed();
+                break;
+            case LocomotionType.Teleport:
+                _soundEffect.enabled = false;
+                break;
+            case LocomotionType.Armswing when leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f || rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f:
+                _soundEffect.enabled = true;
+                GameManager.instance.CalculatePlayerSpeed();
+                break;
+            case LocomotionType.Armswing:
+                _soundEffect.enabled = false;
+                break;
+            default: 
+                _soundEffect.enabled = false;
+                break;
         }
     }
 }
